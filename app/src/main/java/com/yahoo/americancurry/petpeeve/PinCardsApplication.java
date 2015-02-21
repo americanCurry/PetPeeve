7c83840e305da41964dfa698b053938fd794f00d
nandaja <nandaja@yahoo-inc.com>
2015-02-20 16:12:22 -0800

    Parse push notifications

---
+++ app/src/main/java/com/yahoo/americancurry/petpeeve/PinCardsApplication.java
@@ -0,0 +1,41 @@
+package com.yahoo.americancurry.petpeeve;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParsePush;
+import com.parse.SaveCallback;
+
+/**
+ * Created by nandaja on 2/20/15.
+ */
+public class PinCardsApplication extends Application {
+
+    public static final String YOUR_APPLICATION_ID = "NKazbKlKRGU9tBWFi11GaTexVy6HqhHHoBSy8FFk";
+    public static final String YOUR_CLIENT_KEY = "6bhxZmyp6nTMYTpCBRAJm8hn1RwaPEaKyLc5xLi8";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Add your initialization code here
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(this, YOUR_APPLICATION_ID, YOUR_CLIENT_KEY);
+
+        //Subscribe to PARSE push service
+        ParsePush.subscribeInBackground("", new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    Log.d("com.parse.push", "successfully subscribed to the broadcast channel.");
+                } else {
+                    Log.e("com.parse.push", "failed to subscribe for push", e);
+                }
+            }
+        });
+
+    }
+
+}
